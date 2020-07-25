1ad8384d414afd3fe8afc3bbf1f05679ef3a7464
Miura55 <acordion.piano@gmail.com>
2020-07-25 23:25:57 +0900

    startup関数と読み取り結果の出力を追加

--- readTag.py
+++ readTag.py
@@ -1,11 +1,19 @@
 import binascii
 import nfc
 
+def on_startup(target):
+    print('-----------------Waiting for reading-----------------------')
+    print('target:', target)
+    return target
+
 def on_connect(tag):
     for record in tag.ndef.records:
-        print(record)    
+        print(record)
     return True
 
 if __name__ == '__main__':
     with nfc.ContactlessFrontend('usb') as clf:
-        clf.connect(rdwr={'on-connect':on_connect})
+        clf.connect(rdwr={
+            'on-startup':on_startup,
+            'on-connect':on_connect
+        })
